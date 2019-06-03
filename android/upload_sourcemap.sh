bc83420c4d57d39694faf16e4c8e08afdd178d5b
Salma Ali <bluet.salma@gmail.com>
2019-06-03 12:39:11 +0200

    🚑 fix for upload sourcemap script failing on generating a signed APK on a windows machine

--- android/upload_sourcemap.sh
+++ android/upload_sourcemap.sh
@@ -17,7 +17,7 @@ react-native bundle --platform android \
 zip ./android-sourcemap.zip ./android-sourcemap.json
 
 if [ ${INSTABUG_APP_TOKEN} == "YOUR_APP_TOKEN" ]; then
-    echo "Instabug: Looking for Token..."
+    cmd /C echo "Instabug: Looking for Token..."
     if [ ! "${INSTABUG_APP_TOKEN}" ]; then
         INSTABUG_APP_TOKEN=$(grep -r --exclude-dir={node_modules,ios,android} 'Instabug.startWithToken(\"[0-9a-zA-Z]*\"' ./ -m 1 | grep -o '\"[0-9a-zA-Z]*\"' | cut -d "\"" -f 2)
     fi
@@ -29,11 +29,11 @@ fi
 
 
 if [ ! "${INSTABUG_APP_TOKEN}" ] || [ -z "${INSTABUG_APP_TOKEN}" ] || [ "${INSTABUG_APP_TOKEN}" == "YOUR_APP_TOKEN" ];then
-    echo "Instabug: err: INSTABUG_APP_TOKEN not found. Make sure you've added the SDK initialization line Instabug.startWithToken Or added it to the environment variable in the gradle"
+    cmd /C echo "Instabug: err: INSTABUG_APP_TOKEN not found. Make sure you've added the SDK initialization line Instabug.startWithToken Or added it to the environment variable in the gradle"
     exit 0
 else
-    echo "Instabug: Uploading files..."
+    cmd /C echo "Instabug: Uploading files..."
     #Upload ios sourcemap
     curl -X POST 'https://api.instabug.com/api/sdk/v3/symbols_files'  -F "symbols_file=@./android-sourcemap.json"  -F "application_token=${INSTABUG_APP_TOKEN}"  -F "platform=react_native"  -F "os=android" 
-    echo 
+    cmd /C echo ""
 fi
